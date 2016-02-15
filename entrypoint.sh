665236bdf16fd4ae28fce6fd802fbcae19f99926
Per Abich <per.abich@gmail.com>
2016-02-15 07:20:16 +0000

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 
 curl -X PUT -d '{"Datacenter":"dc1", "Node":"postgrey","Address": "postgrey", "Service": {"Service":"postgrey", "Port": 60000}}' http://${CONSUL_PORT_8500_TCP_ADDR}:${CONSUL_PORT_8500_TCP_PORT}/v1/catalog/register
 
-#/consul agent -data-dir=/consul-data -join ${CONSUL_PORT_8301_TCP_ADDR}:${CONSUL_PORT_8301_TCP_PORT} -config-dir=/etc/consul.d/
 exec $@
